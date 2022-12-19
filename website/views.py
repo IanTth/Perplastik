3e860429c7a4c7849dbe2ee6c033984e8fb04316
IanTth <ianrendrick@gmail.com>
2022-12-19 14:13:14 -0300

    Aplicação

---
+++ website/views.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, render_template
+
+views = Blueprint('views', __name__)
+
+# rotas #
+
+@views.route('/')
+def home():
+    return render_template("index.html")
+
+@views.route('produtos')
+def produtos():
+    return render_template('serviços/produtos.html')
+
+@views.route('serviços')
+def serviços():
+    return render_template('serviços/serviços.html')
+    
+@views.route('contatos')
+def contatos():
+    return render_template('contatos.html')
